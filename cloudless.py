8747f7d8124280b9d3de884f693ceb6aa9559e79
chrisgtech <chrisgtech@users.noreply.github.com>
2018-01-04 13:02:00 -0600

    Add cert generation

--- cloudless.py
+++ cloudless.py
@@ -3,6 +3,9 @@ from argparse import ArgumentParser
 from configparser import ConfigParser
 from pathlib import Path
 
+import security
+
+WARNING = f'This only works on Python 3.6'
 CONFIG_FILE = Path('cloudless.ini')
 UNKNOWN = 'UNKNOWN'
 
@@ -13,6 +16,9 @@ def loadconfig():
         group = {}
         group['name'] = UNKNOWN
         options['group'] = group
+        machine = {}
+        machine['name'] = UNKNOWN
+        options['machine'] = machine
         saveconfig(options)
     options.read(config)
     return options
@@ -24,7 +30,14 @@ def saveconfig(options):
         
 def init(options):
     groupname = input('Enter name for new group: ')
+    security.certgen(groupname)
+    return groupname
     
+def addmachine(options):
+    groupname = options['group']['name']
+    machinename = input('Enter name for the machine: ')
+    security.certgen(machinename, groupname)
+    return machinename
 
 def main():
     parser = ArgumentParser()
@@ -33,7 +46,17 @@ def main():
     notsetup = options['group']['name'] == UNKNOWN
     if (notsetup):
         print('No group set, running init')
-        init(options)
+        groupname = init(options)
+        options['group']['name'] = groupname
+        saveconfig(options)
+        return
+    nomachine = options['machine']['name'] == UNKNOWN
+    if (nomachine):
+        print('No machine set, adding machine')
+        machinename = addmachine(options)
+        options['machine']['name'] = machinename
+        saveconfig(options)
+        return
         
 if __name__ == "__main__":
     main()
---
+++ security.py
@@ -0,0 +1,35 @@
+from OpenSSL import crypto, SSL
+from uuid import uuid4
+
+def certgen(name, root=None):
+    private = crypto.PKey()
+    private.generate_key(crypto.TYPE_RSA, 4096)
+    cert = crypto.X509()
+    subject = cert.get_subject()
+    subject.C = 'NA'
+    subject.ST = 'N/A'
+    subject.L = 'N/A'
+    subject.O = 'N/A'
+    subject.OU = 'N/A'
+    subject.CN = name
+    uuid = uuid4().int
+    cert.set_serial_number(uuid)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(68*365*24*60*60)
+    rootcert = cert
+    rootkey = private
+    if root:
+        with open(f'{root}.pem', "rb") as pemfile:
+            rootcert = crypto.load_certificate(crypto.FILETYPE_PEM, pemfile.read())
+        with open(f'{root}-prv.pem', "rb") as pemfile:
+            rootkey = crypto.load_privatekey(crypto.FILETYPE_PEM, pemfile.read())
+    cert.set_issuer(rootcert.get_subject())
+    cert.set_pubkey(private)
+    cert.sign(rootkey, 'sha256')
+
+    with open(f'{name}.pem', "wb") as pemfile:
+        pemfile.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+    with open(f'{name}-prv.pem', "wb") as pemfile:
+        pemfile.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+        pemfile.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, private))
+        
