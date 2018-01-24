8fd6b54efda27573c7b105f9d3a39c84e74c831a
chrisgtech <chrisgtech@users.noreply.github.com>
2018-01-24 10:10:21 -0600

    Add local and public IP lookup

--- net.py
+++ net.py
@@ -1,21 +1,36 @@
 #! python3
 
-import netifaces
+import netifaces, ipgetter
+from ipaddress import ip_address
 
-def getips():
+def ifaceips():
     ips = []
     for iface in netifaces.interfaces():
         addresses = netifaces.ifaddresses(iface)
         if not netifaces.AF_INET in addresses:
             continue
-        print(iface)
+        #print(iface)
         info = addresses[netifaces.AF_INET]
-        address = info[0]['addr']
-        if address == '127.0.0.1':
+        address = ip_address(info[0]['addr'])
+        if address.is_loopback:
+            print('Ignoring loopback {}'.format(address))
             continue
-        print(address)
-        ips.append(address)
+        if address.is_global:
+            print('Ignoring public {}'.format(address))
+            continue
+        #print(address)
+        ips.append(str(address))
     return ips
     
+def publicip():
+    ip = ipgetter.myip()
+    if ip is None or ip == '':
+        return None
+    address = ip_address(ip)
+    if not address.is_global:
+        return publicip()
+    return str(address)
+    
 if __name__ == '__main__':
-    print(getips())
+    print(ifaceips())
+    print(publicip())
